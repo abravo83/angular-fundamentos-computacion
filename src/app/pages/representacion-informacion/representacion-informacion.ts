4d1a129bd2279488059578782762fc8540c74a89
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2026-02-22 20:00:26 +0100

    Agregar compoennte RepresentacionInformacion

---
+++ src/app/pages/representacion-informacion/representacion-informacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-representacion-informacion',
+  imports: [],
+  templateUrl: './representacion-informacion.html',
+  styleUrl: './representacion-informacion.css',
+})
+export class RepresentacionInformacion {
+
+}
