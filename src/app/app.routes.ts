07d7df77f6e8eb17af882abae1bbdb5fd39f3f2a
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2026-02-22 20:01:15 +0100

    Agregar ruta al componente RepresentacionInformacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { RepresentacionInformacion } from './pages/representacion-informacion/representacion-informacion';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
+  {path: 'representacion-informacion', component: RepresentacionInformacion},
   {path: '**', redirectTo: '/home'}
 ];
