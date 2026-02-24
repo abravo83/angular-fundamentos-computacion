ce5681aaf55f3be9202da116aa722fb91283d403
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2026-02-24 06:58:45 +0100

    Agregar enlaces de navegación en la tabla de contenido y mejorar la estructura de encabezados en la sección de Representación de la Información

--- src/app/pages/representacion-informacion/representacion-informacion.ts
+++ src/app/pages/representacion-informacion/representacion-informacion.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-representacion-informacion',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './representacion-informacion.html',
   styleUrl: './representacion-informacion.css',
 })
