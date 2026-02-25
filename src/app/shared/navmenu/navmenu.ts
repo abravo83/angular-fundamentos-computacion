e255a5c77e04c6b8016e7ead59b8b05375e0ffeb
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2026-02-25 23:44:51 +0100

    Importar RouterLink a componente navmenu

--- src/app/shared/navmenu/navmenu.ts
+++ src/app/shared/navmenu/navmenu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navmenu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navmenu.html',
   styleUrl: './navmenu.css',
 })
